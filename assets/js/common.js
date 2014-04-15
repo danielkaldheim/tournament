41aff98e8838fd62d1197597397d7c9467763cff
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2014-04-15 15:30:02 +0200

    Try to stop app switching to safari in webappmode

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,11 @@
 $(window).on('load', function () {
     $('select').selectpicker();
 });
+
+
+$(function() {
+	$('a').click(function() {
+		document.location = $(this).attr('href');
+		return false;
+	});
+});
